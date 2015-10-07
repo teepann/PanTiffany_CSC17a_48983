50d181c54c85c61368c602c70d8513563a18df39
Tiffany Pan <tfunnyblue@yahoo.com>
2015-10-07 00:24:56 -0700

    updated

---
+++ Hmwk/Structure_DataBase_Sort_Indexed_Conversion/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=None-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_ARTIFACT_NAME_Debug=structure_database_sort_indexed
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/structure_database_sort_indexed
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_PACKAGE_NAME_Debug=structuredatabasesortindexed.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/structuredatabasesortindexed.tar
+# Release configuration
+CND_PLATFORM_Release=None-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_ARTIFACT_NAME_Release=structure_database_sort_indexed
+CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/structure_database_sort_indexed
+CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_PACKAGE_NAME_Release=structuredatabasesortindexed.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/structuredatabasesortindexed.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ upload.bat
@@ -0,0 +1,5 @@
+git add --all
+git config --global user.name "Tiffany Pan"
+git config --global user.email tfunnyblue@yahoo.com
+git commit -m "Commit"
+git push origin master
