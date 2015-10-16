4ea1ab09d0877ee3f0a957729757d092b3a1e234
teepann <tfunnyblue@yahoo.com>
2015-10-15 20:41:20 -0700

    Update main.cpp

--- Hmwk/091315 Review Assignment/prob_5_11/main.cpp
+++ Hmwk/091315 Review Assignment/prob_5_11/main.cpp
@@ -1,49 +1,61 @@
-/* 
- * prob_5_11.cpp 
- * Author: Tiffany Pan
- * Created on September 8, 2015, 3:44 PM
- * Purpose: predict size of a population given initial number, avg daily increase, and number of days
- * Homework: Complete problem 5.11
- */
+/*
+* File: main.cpp
+* Author: Tiffany Pan
+* Date: September 13, 2015
+* Assignment: Problem 5.11 Population
+* Directions: Write a program that will predict the size of a population of organisms. 
+* The program should ask the user for the starting number of organisms, their average 
+* daily popula-tion increase (as a percentage), and the number of days they will multiply. 
+* A loop should display the size of the population for each day.
+* Input Validation: Do not accept a number less than 2 for the starting size of the 
+* population. Do not accept a negative number for average daily population increase. 
+* Do not accept a number less than 1 for the number of days they will multiply.
+*/
 
 //System Libraries
-#include <iostream> //input/output stream Library
-#include <iomanip> //format Library
-using namespace std; //utilize standard name-space directly
+#include <iostream>
+using namespace std;
 
 //User Libraries
 
 //Global Constants
 
-//Function Prototypes 
+//Function Prototypes
 
-/*
- * Population increase
- */
+//Execution begins here
 int main(int argc, char** argv) {
-    int numOrganisms, numDays, population;
-    float avgInc;
-    
-    //ask user for starting number of organisms
-    do{cout<<"How many organisms to start?"<<endl;
-    cin>>numOrganisms;
-    }while(numOrganisms<2);
-    population=numOrganisms;
-    //ask user for average daily population increase in percentage non negative
-    do{cout<<"Average daily population increase percentage?"<<endl;
-    cin>>avgInc;
-    }while(avgInc<0);
-    //ask user for number of days they will multiply that is greater than 1
-    do{cout<<"How many days will they multiply?"<<endl;
-    cin>>numDays;
-    }while(numDays<1);
-    
-    //loop that displays the day and the size of the population for each day
-    for(int i=0;i<=numDays;i++)
-    {
-        cout<<"Day "<<i<<": "<<population<<endl;
-        population+=population*avgInc/100;
-    }
-    return 0;
-}
+	//Variable declarations
+	int numOrganisms, numDays, population; 
+	float avgInc; //average increase as percentage
 
+	//prompt user for initial population
+	//does not accept a number less than 2
+	do{
+		cout << "How many organisms to start?" << endl;
+		cin >> numOrganisms;
+	} while (numOrganisms<2);
+	population = numOrganisms;
+
+	//prompt user for average daily population increase in percentage
+	//checks that it is non negative value
+	do{
+		cout << "Average daily population increase percentage?" << endl;
+		cin >> avgInc;
+	} while (avgInc<0);
+
+	//prompts user for number of days they will multiply that is greater than 1
+	do{
+		cout << "How many days will they multiply?" << endl;
+		cin >> numDays;
+	} while (numDays<1);
+
+	//loop that displays the day and the size of the population for each day
+	for (int i = 0; i <= numDays; i++)
+	{
+		cout << "Day " << i << ": " << population << endl;
+		population += population*avgInc / 100;
+	}
+
+	system("pause");
+	return 0;
+}
