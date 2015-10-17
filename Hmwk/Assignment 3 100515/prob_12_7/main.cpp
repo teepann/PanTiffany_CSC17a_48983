d2e95585b80b2bc3f644007cb9cdc44a1891745f
teepann <tfunnyblue@yahoo.com>
2015-10-17 12:34:40 -0700

    Signed-off-by: teepann <tfunnyblue@yahoo.com>

--- Hmwk/Assignment 3 100515/prob_12_7/main.cpp
+++ Hmwk/Assignment 3 100515/prob_12_7/main.cpp
@@ -13,6 +13,8 @@
 
 //System Libraries
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 //User Libraries
@@ -20,15 +22,65 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
+string changeCase(string);
 
 //Execution begins here
 int main(int argc, char** argv) {
    //Variable declarations
-
+    //delimiter of a sentence
+    const char DELIMITER = '.';
+    //string delimiter of a sentence
+    const string SDELIMITER = ".";
+    //input file name
+    string ifileName;
+    //output file name
+    string ofileName;
+    //input file
+    ifstream ifile;
+    //output file
+    ofstream ofile;
+    //sentence from file
+    string sentence;
+    
+    cout<< "Please enter file name: ";
+    getline(cin, ifileName);
+    
+    cout << "Please enter an output file name: ";
+    getline(cin, ofileName);
+    
+    ifile.open(ifileName.c_str(), ios::in);
+    ofile.open(ofileName.c_str(), ios::out);
+    
+    if(ifile && ofile){
+        while(getline(ifile, sentence, DELIMITER)){
+            sentence.append(SDELIMITER);
+            ofile << changeCase(sentence);
+        }
+        ifile.close();
+        ofile.close();
+        
+    } else 
+        cerr << "File IO error!";
    return 0;
 }
 
 /* 
- * Definition of function.
- *
+ * Definition of function changeCase
+ * changes all the letters to lowercase except the first letter of each sentence
+ * parameter: string
+ * returns the correct sentence
  */
+string changeCase(string str){
+    int i = 0;
+    
+    while((i < str.length()) && !isalpha(str[i])){
+        i++;
+    }
+    str[i] = toupper(str[i]);
+    
+    //lowercase for rest of sentence
+    for (i = i + 1; i < str.length(); i++) {
+        str[i] = tolower(str[i]);
+    }
+    return str;
+}
--- Hmwk/Assignment 3 100515/prob_12_8/main.cpp
+++ Hmwk/Assignment 3 100515/prob_12_8/main.cpp
@@ -4,7 +4,7 @@
  * Date: October 1, 2015
  * Assignment: Problem 12.8
  * Directions: write a function named arrayToFile. The function should accept 3 
- * arguments: the name of afile, a pointer to an int array,
+ * arguments: the name of a file, a pointer to an int array,
  * the size of the array. the function should open the specified
  * file in binary mode, write the contents of the array to the file,
  * and then close the file.
@@ -21,6 +21,7 @@
 
 //System Libraries
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 //User Libraries
@@ -28,15 +29,58 @@ using namespace std;
 //Global Constants
 
 //Function Prototypes
+void arrayToFile (const char*, int*, const int);
+void fileToArray (const char*, int*, const int);
 
 //Execution begins here
 int main(int argc, char** argv) {
    //Variable declarations
-
+    const char* FILE_NAME = "file.dat";
+    const int SIZE = 10;    
+    int ary[SIZE] = {23, 67, 12, 89, 23, 12, 78, 90, 22, 88};
+    int *result;
+    
+    //read array to file
+    arrayToFile(FILE_NAME, ary, SIZE);
+    //file contents into array
+    fileToArray(FILE_NAME, ary, SIZE);
+    
+    //print out array
+    for(int i = 0; i< SIZE; i++){
+        cout << result[i] << " ";
+    }
    return 0;
 }
 
 /* 
- * Definition of function.
- *
+ * Definition of function arrayToFile
+ * opens the given file in binary mode,
+ * write the contents of array to the file and closes it
+ * parameter: name of a file, pointer to int array, and size of array
  */
+void arrayToFile (const char* fileName, int* ary, const int size){
+    fstream aryToFile(fileName, ios::out | ios::binary);
+    if(aryToFile){
+        aryToFile.write(reinterpret_cast<char*>(ary), size*sizeof(int));
+        aryToFile.close();
+    }
+    else
+        cerr< "File I/O error!";
+}
+
+/* 
+ * Definition of function fileToArray
+ * opens the given file in binary mode,
+ * reads contents into array and closes it
+ * parameter: name of a file, pointer to int array, and size of array
+ */
+void fileToArray (const char* fileName, int* ary, const int size){
+    fstream fileToAry (fileName, ios::in|ios::binary);
+    
+    if(fileToAry) {
+        fileToAry.read(reinterpret_cast<char*>(ary), size*sizeof(int));
+        fileToAry.close();
+    }
+    else
+        cerr <<"File IO error!";
+}
